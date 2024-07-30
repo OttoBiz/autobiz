9fe0bd3c9344df6fffed04c4acf7e6dc438cbd87
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2024-07-30 21:58:00 +0100

    improve agent flow

--- backend/chatbot/agents/product_agent.py
+++ backend/chatbot/agents/product_agent.py
@@ -88,58 +88,77 @@ async def run_product_agent(
                                     chat_history:{}
     }
     """
-    products = user_state.get("products", {})
-    product = products.get(product_name, {})
-    retrieved_products_info = product.get("retrieved_results", None)
-    
-    db_queried = product.get("db_queried", False)
-    # print("Product name: ", product_name)
-    # print("category: ", product_category)
-    
-    if retrieved_products_info is None and not db_queried:
-        retrieved_products_info = await get_products(product_name, product_category, **kwargs )
-        # products["product_name"] = product_name
-        # print("Products: ", products)
-        # print("Retrieved products: ", retrieved_products_info)
-        products[product_name] = {}
-        products[product_name]["retrieved_results"]= retrieved_products_info
-        products[product_name]["db_queried"] = True
-        user_state["products"] = products
-       
-    result_match = await evaluator_chain.ainvoke({
-        "available_products": retrieved_products_info,
-        "customer_enquiry": customer_message
-    })
-    
-    print("Retrieved products info: ", retrieved_products_info)
+    if product_name != "NONE":
+        products = user_state.get("products", {})
+        product = products.get(product_name, {})
+        retrieved_products_info = product.get("retrieved_results", None)
+        
+        db_queried = product.get("db_queried", False)
+        # print("Product name: ", product_name)
+        # print("category: ", product_category)
+        
+        if retrieved_products_info is None and not db_queried:
+            retrieved_products_info = await get_products(product_name, product_category, **kwargs )
+            # products["product_name"] = product_name
+            # print("Products: ", products)
+            # print("Retrieved products: ", retrieved_products_info)
+            products[product_name] = {}
+            products[product_name]["retrieved_results"]= retrieved_products_info
+            products[product_name]["db_queried"] = True
+            user_state["products"] = products
+        
+        result_match = await evaluator_chain.ainvoke({
+            "available_products": retrieved_products_info,
+            "customer_enquiry": customer_message
+        })
+        
+        print("Retrieved products info: ", retrieved_products_info)
 
-    result_match = json.loads(result_match.additional_kwargs.get("tool_calls")[0].get("function")["arguments"])
-    print("Result match: ", result_match)
-    
-    
-    if intent== "purchase" and len(retrieved_products_info) > 1:
-        business_information = user_state.get("business_information")
-        print("Business information: ", business_information)
-        bank_details = f"Bank name: {business_information['bank_name']}, Bank account name: {business_information['bank_account_name']}, Bank account number: {business_information['bank_account_number']}."
-    else:
-        bank_details = ""
+        result_match = json.loads(result_match.additional_kwargs.get("tool_calls")[0].get("function")["arguments"])
+        print("Result match: ", result_match)
         
         
-    chain_input = {
-            "vendor_bank_details": bank_details, #TODO: add account or payment details to businesses.
-            "customer_message": customer_message,
-            # "product_name": product_name,
-            "product_category": product_category,
-            "intent": intent,
-            "tone": tone,
-            "product_attributes": product_attributes,
-            "instruction" : f"Further instructions concerning user: {result_match['instruction']}" if result_match["product_match"] != "NO_MATCH" and result_match.get("available_products") else ""
+        if intent== "purchase" or len(retrieved_products_info) > 1:
+            business_information = user_state.get("business_information")
+            print("Business information: ", business_information)
+            bank_details = f"Bank name: {business_information['bank_name']}, Bank account name: {business_information['bank_account_name']}, Bank account number: {business_information['bank_account_number']}."
+            print(bank_details)
+        else:
+            bank_details = ""
             
-        }
-    
-    # print("Result match: " , result_match)
-    chain_input["available_products"] = result_match.get("available_products", "NO PRODUCT IS AVAILABLE CURRENTLY") 
-    # print(chain_input)
-    return await product_agent.ainvoke(
-        chain_input
-    ), user_state
+            
+        chain_input = {
+                "vendor_bank_details": bank_details, #TODO: add account or payment details to businesses.
+                "customer_message": customer_message,
+                # "product_name": product_name,
+                "product_category": product_category,
+                "intent": intent,
+                "tone": tone,
+                "product_attributes": product_attributes,
+                "instruction" : f"Further instructions concerning user: {result_match['instruction']}" if result_match["product_match"] != "NO_MATCH" and result_match.get("available_products") else ""
+                
+            }
+        
+        # print("Result match: " , result_match)
+        chain_input["available_products"] = result_match.get("available_products", "NO PRODUCT IS AVAILABLE CURRENTLY") 
+        # print(chain_input)
+        return await product_agent.ainvoke(
+            chain_input
+        ), user_state
+        
+    else:
+        chain_input = {
+                "vendor_bank_details": "", #TODO: add account or payment details to businesses.
+                "customer_message": customer_message,
+                # "product_name": product_name,
+                "product_category": "",
+                "intent": "",
+                "tone": "",
+                "product_attributes": "",
+                "instruction" : f"Further instructions: Politely ask user to provide information about the name of the product he is interested in"                
+            }
+        
+        # print("Result match: " , result_match)
+        chain_input["available_products"] =  "NO PRODUCT WAS MENTIONED IN THE customer_message" 
+        
+        return product_agent.invoke(chain_input), user_state
