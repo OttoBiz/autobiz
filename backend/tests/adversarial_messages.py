3acb3355103a6a9c50b642197dcd8e3336430b8e
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2024-07-30 15:41:04 +0100

    create test messages for test cases

---
+++ backend/tests/adversarial_messages.py
@@ -0,0 +1,5 @@
+initial_messages = [
+    "okay, I want to buy one. what is the best price you can give me?",
+    "Hello, I want to buy a samsung phone. which one do you have?",
+    "okay, which phones do you have available?"
+]
