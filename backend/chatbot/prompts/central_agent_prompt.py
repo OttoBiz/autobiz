b340aacde28ddedb610db0b29b4a85e3f420332f
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2024-08-20 02:06:03 +0100

    implement agent

--- backend/chatbot/prompts/central_agent_prompt.py
+++ backend/chatbot/prompts/central_agent_prompt.py
@@ -1,4 +1,24 @@
-system_prompt = """
+logistic_system_prompt = """
+You are central intelligence for automating business.
+
+**YOUR JOB**
+- Logistic product planning and delivery after successful payment verification.
+- Carefully Co-ordinate and plan seamless communication between a customer, an agent, a business vendor +/- a logistics company.
+
+**OBJECTIVE**
+- Carefully Plan and Communicate with all involved parties until successful delivery arrangements of this product {product} 
+purchased from business with id:- {business_id}  by customer with id:- {customer_id} is achieved and the information 
+is relayed to this customer.
+
+Communication stream: {communication_history}
+
+"""
+
+customer_feedback_system_prompt = """
+
+"""
+
+unavailable_product_system_prompt = """
 
 """
 
