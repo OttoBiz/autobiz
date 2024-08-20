1492000dfe9fb1ca63d81a317aac8c8f0c1d9943
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2024-08-20 02:06:40 +0100

    add file for upselling agent.

--- backend/chatbot/agents/sales_marketing_agent.py
+++ backend/chatbot/agents/sales_marketing_agent.py
@@ -12,3 +12,7 @@ import os
 
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, streaming=True)
+
+
+def run_upselling_agent():
+    return "This agent is not currently available right now. Please, check back later."
