0a43a344b1e87b369c2da56afc8751c7c3857543
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2024-09-17 16:42:31 +0100

    implement business_chat function

--- backend/chatbot/agents/business_chat_interface.py
+++ backend/chatbot/agents/business_chat_interface.py
@@ -1,8 +1,20 @@
-from backend.db.cache_utils import get_user_state, modify_user_state, delete_user_state
+from backend.db.cache_utils import get_business_state, modify_business_state, delete_user_state
 from backend.db.db_utils import *
 from .central_agent import run_central_agent
 from .central_agent_utils import create_structured_input
 
-async def business_chat(user_request, debug=False):
-    response = ""
+
+async def business_chat(business_request, debug=False):
+    
+    # product_name, product_price, amount_paid, customer_name, 
+    #                              bank_account_number, bank_name, customer_message, **kwargs
+                                 
+    agent_input = await create_structured_input(sender=business_request.sender, recipient="agent", message=business_request.message, 
+                                                product_name=business_request.product_name, price= business_request.product_price, 
+                                                customer_id= business_request.user_id, business_id = business_request.vendor_id,
+                                                logistic_id = business_request.logistic_id, message_type=business_request.message_type,
+                                    )
+        
+    response = await run_central_agent(agent_input)
+    
     return response
