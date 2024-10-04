e5de87a68c6fb01f5bbc6ce61f082caa85d606b7
David Okpare <okparedave@gmail.com>
2024-10-04 15:52:19 +0100

    update config with db url

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,18 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
-from dotenv import load_dotenv
 
-from .config import (
-    DATABASE_USERNAME,
-    DATABASE_PASSWORD,
-    DATABASE_HOST,
-    DATABASE_NAME
-)
-
-load_dotenv()
-
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
+from .config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
