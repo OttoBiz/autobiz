830bd9a8e94f590bd41f77a3ff6452ad83ff091e
David Okpare <okparedave@gmail.com>
2024-10-04 15:18:59 +0100

    enable using db url

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
+from dotenv import load_dotenv
 
 from .config import (
     DATABASE_USERNAME,
@@ -10,8 +12,9 @@ from .config import (
     DATABASE_NAME
 )
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
