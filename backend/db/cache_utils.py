017153f0b4718793afa4f9f119bba123cc9c9a79
David Okpare <okparedave@gmail.com>
2024-10-16 13:52:31 +0100

    Refactor exception to be more detailed

--- backend/db/cache_utils.py
+++ backend/db/cache_utils.py
@@ -35,8 +35,8 @@ async def modify_user_state(user_id: str, vendor_id: str, user_state: dict, sess
     print(key)
     try:
         redis_conn.set(key, user_state)
-    except:
-        print("Failed to store key")
+    except Exception as e:
+        print("Failed to store key: ", e)
 
 async def delete_user_state(user_id: str, vendor_id: str) -> None:
     key = f"user_state:{user_id}:{vendor_id}"
