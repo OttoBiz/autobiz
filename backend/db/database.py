6436e9056a1999345973a32646d0d28c5a003894
DaveOkpare <okparedave@gmail.com>
2025-04-27 23:16:02 +0100

    update psycopg2 dependency to psycopg2-binary and remove unused contextmanager import

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
 
 from .config import SQLALCHEMY_DATABASE_URL
 
@@ -11,8 +10,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-
-@contextmanager
 def get_db():
     db = SessionLocal()
     try:
