2c2081b66c0169847882f0783fff729d678e1cd9
DaveOkpare <okparedave@gmail.com>
2025-05-10 14:42:59 +0100

    refactor database.py and db_utils.py to clean up commented-out code and improve readability

--- backend/db/database.py
+++ backend/db/database.py
@@ -5,11 +5,10 @@ from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
 from .config import SQLALCHEMY_DATABASE_URL
+from .models import Base
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 @contextmanager
 def get_db():
--- backend/db/db_utils.py
+++ backend/db/db_utils.py
@@ -24,24 +24,24 @@ async def get_products(
     with get_db() as db:
         products_query = db.query(Product)
 
-        if name:
-            search_term = f"%{name}%"
-            products_query = products_query.filter(
-                or_(
-                    Product.product_name.ilike(search_term),
-                    Product.product_description.ilike(search_term),
-                    Product.tags.ilike(search_term),
-                )
-            )
+        # if name:
+        #     search_term = f"%{name}%"
+        #     products_query = products_query.filter(
+        #         or_(
+        #             Product.product_name.ilike(search_term),
+        #             Product.product_description.ilike(search_term),
+        #             Product.tags.ilike(search_term),
+        #         )
+        #     )
 
-        # if category:
-        #     products_query = products_query.filter(Product.product_category == category)
+        # # if category:
+        # #     products_query = products_query.filter(Product.product_category == category)
 
-        if min_price is not None:
-            products_query = products_query.filter(Product.price >= min_price)
+        # if min_price is not None:
+        #     products_query = products_query.filter(Product.price >= min_price)
 
-        if max_price is not None:
-            products_query = products_query.filter(Product.price <= max_price)
+        # if max_price is not None:
+        #     products_query = products_query.filter(Product.price <= max_price)
 
         # Dynamically add filters based on kwargs
         # for key, value in kwargs.items():
@@ -70,16 +70,16 @@ async def get_business_info(
     with get_db() as db:
         business_query = db.query(Business)
         
-        business_query = business_query.filter(
-            or_(
-                Business.ig_page.ilike(vendor_id),
-                Business.facebook_page.ilike(vendor_id),
-                Business.twitter_page.ilike(vendor_id),
-                Business.phone_number.ilike(vendor_id),
-                Business.email.ilike(vendor_id),
-                Business.tiktok.ilike(vendor_id)
-            )
-        )
+        # business_query = business_query.filter(
+        #     or_(
+        #         Business.ig_page.ilike(vendor_id),
+        #         Business.facebook_page.ilike(vendor_id),
+        #         Business.twitter_page.ilike(vendor_id),
+        #         Business.phone_number.ilike(vendor_id),
+        #         Business.email.ilike(vendor_id),
+        #         Business.tiktok.ilike(vendor_id)
+        #     )
+        # )
         # business_query = business_query.filter(Business.tiktok == "2347000000004")
         # business_query = business_query.filter(Business.phone_number == vendor_id)
       
@@ -95,61 +95,61 @@ def to_dict(db_object):
     return dict_obj
 
 
-def search_products(query: str, limit: int = 10, offset: int = 0) -> List[Product]:
-    with get_db() as db:
-        try:
-            formatted_query = " | ".join(query.split())  # Use OR instead of AND
+# def search_products(query: str, limit: int = 10, offset: int = 0) -> List[Product]:
+#     with get_db() as db:
+#         try:
+#             formatted_query = " | ".join(query.split())  # Use OR instead of AND
             
-            sql_query = text("""
-                SELECT *
-                FROM products
-                WHERE ts_vector @@ to_tsquery('english', :query)
-                ORDER BY ts_rank(ts_vector, to_tsquery('english', :query)) DESC
-                LIMIT :limit OFFSET :offset
-            """)
+#             sql_query = text("""
+#                 SELECT *
+#                 FROM products
+#                 WHERE ts_vector @@ to_tsquery('english', :query)
+#                 ORDER BY ts_rank(ts_vector, to_tsquery('english', :query)) DESC
+#                 LIMIT :limit OFFSET :offset
+#             """)
             
-            results = db.execute(sql_query, {
-                "query": formatted_query,
-                "limit": limit,
-                "offset": offset
-            })
+#             results = db.execute(sql_query, {
+#                 "query": formatted_query,
+#                 "limit": limit,
+#                 "offset": offset
+#             })
             
-            # Get the column names of the Product model
-            product_columns = set(inspect(Product).columns.keys())
+#             # Get the column names of the Product model
+#             product_columns = set(inspect(Product).columns.keys())
             
-            # Filter out any columns that aren't in the Product model
-            products = [
-                Product(**{k: v for k, v in row._asdict().items() if k in product_columns})
-                for row in results
-            ]
+#             # Filter out any columns that aren't in the Product model
+#             products = [
+#                 Product(**{k: v for k, v in row._asdict().items() if k in product_columns})
+#                 for row in results
+#             ]
             
-            if not products:
-                print("No results found. Checking ts_vector content...")
-                check_query = text("""
-                    SELECT id, product_name, ts_vector::text
-                    FROM products
-                    LIMIT 5
-                """)
-                check_results = db.execute(check_query)
-                for row in check_results:
-                    print(f"ID: {row.id}, Name: {row.product_name}, ts_vector: {row.ts_vector}")
+#             if not products:
+#                 print("No results found. Checking ts_vector content...")
+#                 check_query = text("""
+#                     SELECT id, product_name, ts_vector::text
+#                     FROM products
+#                     LIMIT 5
+#                 """)
+#                 check_results = db.execute(check_query)
+#                 for row in check_results:
+#                     print(f"ID: {row.id}, Name: {row.product_name}, ts_vector: {row.ts_vector}")
             
-            return products
-        except ProgrammingError as e:
-            print(f"An error occurred: {str(e)}")
-            return []
+#             return products
+#         except ProgrammingError as e:
+#             print(f"An error occurred: {str(e)}")
+#             return []
 
     
     
     
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # Usage
-    search_results = search_products("i want to buy a gaming laptop", limit=15)
-    if search_results:
-        for product in search_results:
-            print(f"Product: {product.product_name}")
-            print(f"Description: {product.product_description}")
-            print(f"Price: ${product.price}")
-            print("---")
-    else:
-        print("No results found.")
+    # search_results = search_products("i want to buy a gaming laptop", limit=15)
+    # if search_results:
+    #     for product in search_results:
+    #         print(f"Product: {product.product_name}")
+    #         print(f"Description: {product.product_description}")
+    #         print(f"Price: ${product.price}")
+    #         print("---")
+    # else:
+    #     print("No results found.")
