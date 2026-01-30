ff79435c349f1a16680faf4eecad96e2394f3cb5
daveokpare <okparedave@gmail.com>
2026-01-30 17:23:22 +0100

    reformat struct

--- app/backend/struct.py
+++ app/backend/struct.py
@@ -1,32 +1,36 @@
-from pydantic import BaseModel
-from typing import Optional, Dict, Union, Any
 from datetime import datetime
+from typing import Any, Dict, Optional, Union
+
+from pydantic import BaseModel
 
 
-class customer(BaseModel):
+class Customer(BaseModel):
     id: str
     name: Optional[str] = None
     phone: Optional[str] = None
     address: Optional[str] = None
 
+
 class BankAccount(BaseModel):
     name: Optional[str] = None
     number: Optional[str] = None
-    
+
+
 class Vendor(BaseModel):
     id: str
     name: Optional[str] = None
     phone: Optional[str] = None
     address: Optional[str] = None
     bank_account: Optional[BankAccount] = None
-    
+
+
 class Logistics(BaseModel):
     id: str
     name: Optional[str] = None
     phone: Optional[str] = None
     address: Optional[str] = None
     bank_account: Optional[BankAccount] = None
-    
+
 
 class Product(BaseModel):
     id: str
@@ -36,26 +40,29 @@ class Product(BaseModel):
     has_paid: bool = False
     metadata: Optional[Dict[str, Any]] = None
 
+
 class CentralAgentInput(BaseModel):
     """Input for central agent"""
-    sender: str  # ["Agent", "Customer", "Vendor", "Logistics"]
-    recipient: str # ["Agent", "Customer", "Vendor", "Logistics"]
-    business: Optional[Vendor] =None
-    customer: Optional[customer] =None
-    logistic: Optional[Logistics] =None
-    message: str
-    product: Optional[Product] =None
 
-    
+    sender: str  # ["Agent", "Customer", "Vendor", "Logistics"]
+    recipient: str  # ["Agent", "Customer", "Vendor", "Logistics"]
+    business: Optional[Vendor] = None
+    customer: Optional[Customer] = None
+    logistic: Optional[Logistics] = None
+    message: str
+    product: Optional[Product] = None
+
     def to_dict(self):
         return self.dict()
-    
+
+
 class ProductAgentInput(BaseModel):
     customer_message: str
     product_name: str
     product_category: str
     intent: Optional[str] = "enquiry"
-    
+
+
 class UpsellingAgentInput(BaseModel):
     product: str
     intent: Optional[str] = "inquired"
@@ -69,17 +76,19 @@ class CustomerComplaintAgent(BaseModel):
     miscellaneous: str
     customer_id: Optional[str] = "09071536199"
     business_id: str
-    
+
+
 class PaymentVerifcationAgent(BaseModel):
     product_name: str
     customer_id: Optional[str] = "09071536199"
     business_id: str
     product_price: str
     amount_paid: str
-    customer_name: Optional[str] = "Bode Thomas" 
+    customer_name: Optional[str] = "Bode Thomas"
     bank_account_number: Optional[str] = "120507869"
     bank_name: Optional[str] = "GTBank"
-    
+
+
 class UserRequest(BaseModel):
     user_id: str
     vendor_id: str
@@ -99,11 +108,20 @@ class BusinessRequest(BaseModel):
     product_price: Optional[str]
     message_type: str
     msg_date_time: Optional[Union[datetime, str]] = None
-    
+
+
 class AgentRequest(BaseModel):
     user_id: str
     vendor_id: Optional[str] = None
     logistic_id: Optional[str] = None
     agent: str
     message: str
-    agent_input: Optional[Union[CentralAgentInput, CustomerComplaintAgent, PaymentVerifcationAgent, UpsellingAgentInput, ProductAgentInput]] = None
+    agent_input: Optional[
+        Union[
+            CentralAgentInput,
+            CustomerComplaintAgent,
+            PaymentVerifcationAgent,
+            UpsellingAgentInput,
+            ProductAgentInput,
+        ]
+    ] = None
